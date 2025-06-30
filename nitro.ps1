4092585ec1750928fbd6bfbb2bc9a1f6e3eda730
joaodanielcs <joaodanielcs@gmail.com>
2025-06-30 00:15:53 -0300

    Update nitro.ps1

--- nitro.ps1
+++ nitro.ps1
@@ -1,22 +1,27 @@
-$pasta = "$env:TEMP\NitroInstall"
-New-Item -ItemType Directory -Path $pasta -Force | Out-Null
-$parte1 = "$pasta\nitro_pro11_x64.z01"
-$parte2 = "$pasta\nitro_pro11_x64.z02"
-$arquivoZip = "$pasta\nitro_pro11_x64.zip"
-curl.exe -sS -L "https://drive.google.com/uc?export=download&id=1T075cvLtIqr4UNKRovMwtLofZyyL4F3E" -o $parte1
-curl.exe -sS -L "https://drive.google.com/uc?export=download&id=1BgXaHxwxQuzOao_O0U8cSGuvjPCtOpTw" -o $parte2
-Rename-Item -Path $parte2 -NewName "nitro_pro11_x64.zip" -Force
-$winrar = "${env:ProgramFiles(x86)}\WinRAR\WinRAR.exe"
+$ProgressPreference = 'SilentlyContinue';
+$pasta = "$env:TEMP\NitroInstall";
+New-Item -ItemType Directory -Path $pasta -Force | Out-Null;
+$parte1 = "$pasta\nitro_pro11_x64.z01";
+$parte2 = "$pasta\nitro_pro11_x64.z02";
+$arquivoZip = "$pasta\nitro_pro11_x64.zip";
+curl.exe -sS -L "https://drive.google.com/uc?export=download&id=1T075cvLtIqr4UNKRovMwtLofZyyL4F3E" -o $parte1;
+curl.exe -sS -L "https://drive.google.com/uc?export=download&id=1BgXaHxwxQuzOao_O0U8cSGuvjPCtOpTw" -o $parte2;
+Rename-Item -Path $parte2 -NewName "nitro_pro11_x64.zip" -Force;
+$winrar = "${env:ProgramFiles(x86)}\WinRAR\WinRAR.exe";
 if (!(Test-Path $winrar)) {
-    $winrar = "${env:ProgramFiles}\WinRAR\WinRAR.exe"
+    $winrar = "${env:ProgramFiles}\WinRAR\WinRAR.exe";
 } else {
-  irm https://gist.githubusercontent.com/joaodanielcs/9c425be977d1902a4ccdd4808c1f7392/raw/6671a1110f67e2f7822627f5eae62acccc67e07e/winrar.ps1 | iex
-  $winrar = "${env:ProgramFiles}\WinRAR\WinRAR.exe"
-}
-& $winrar x -ibck "$arquivoZip" "$pasta\" | Out-Null
-$msi = Get-ChildItem -Path $pasta -Filter *.msi -Recurse | Select-Object -First 1
+  irm https://gist.githubusercontent.com/joaodanielcs/9c425be977d1902a4ccdd4808c1f7392/raw/6671a1110f67e2f7822627f5eae62acccc67e07e/winrar.ps1 | iex;
+  $winrar = "${env:ProgramFiles}\WinRAR\WinRAR.exe";
+};
+& $winrar x -ibck "$arquivoZip" "$pasta\" | Out-Null;
+$msi = Get-ChildItem -Path $pasta -Filter *.msi -Recurse | Select-Object -First 1;
 if ($msi) {
-    Start-Process msiexec -ArgumentList "/i `"$($msi.FullName)`" /qn" -Wait
-    Remove-Item $msi.FullName -Force -ErrorAction SilentlyContinue
-}
-Remove-Item $pasta -Recurse -Force -ErrorAction SilentlyContinue
+    Start-Process msiexec -ArgumentList "/i `"$($msi.FullName)`" /qn" -Wait;
+    Remove-Item $msi.FullName -Force -ErrorAction SilentlyContinue;
+};
+Remove-Item $pasta -Recurse -Force -ErrorAction SilentlyContinue;
+Remove-Item "$env:TEMP\NitroPDF" -Recurse -Force -ErrorAction SilentlyContinue;
+
+$ProgressPreference = 'Continue';
+Remove-Item "C:\Users\Public\Desktop\Nitro Pro.lnk" -Force -ErrorAction SilentlyContinue;
